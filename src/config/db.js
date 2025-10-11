ed560a55e23079d5700fd1f4007f3061d770a8b4
Andre07G <eleal087@gmail.com>
2025-10-11 18:36:47 -0500

    feat: :sparkles: Add MongoDB connection module Created db.js with asynchronous functions connectDB and getDB to handle MongoDB connection and database access. by @Andre07g

---
+++ src/config/db.js
@@ -0,0 +1,26 @@
+import { MongoClient } from "mongodb";
+import 'dotenv/config';
+
+const uri = process.env.MONGO_URI
+const db_name = process.env.DB_NAME
+
+const client = new MongoClient (uri);
+let db;
+
+export async function connectDB() {
+    try {
+        await cliente.connect();
+        console.log("DB connected!");
+        db = client.db(db_name);
+    } catch (error) {
+        console.error("Error connecting to the DB", error)
+    }
+}
+
+export async function getDB() {
+    if(!db){
+        throw new Error("Has not connected with the DB!");
+    }
+    return db;
+}
+
