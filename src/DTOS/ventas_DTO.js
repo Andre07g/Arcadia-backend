7778b2da8c46cc8f2393342d791957bd7fc52f21
Sebas404040 <delgadopalitogarbage@gmail.com>
2025-10-12 17:26:31 -0500

    feat: :label: DTO type
    Sales dto type created succesfully
    
    none

---
+++ src/DTOS/ventas_DTO.js
@@ -0,0 +1,9 @@
+import {param, body} from "express-validator";
+
+export const crerVenta_dto = [
+    body("fecha").isISO8601().toDate().withMessage("Date has to be ISO8601"),
+    body("productos").isArray({min: 1}).withMessage("Minimum 1 product is required"),
+    body("productos.*.nombre").isString().trim().notEmpty().withMessage("Product name is required"),
+    body("productos.*.precio").isFloat({min: 1}).withMessage("Product price is required"),
+    body("productos.*.cantidad").isInt({min: 1}).withMessage("Product quantity is required"),
+]
