52a8491cfd34fbe755197079a213a14affc7f21c
Sebas404040 <delgadopalitogarbage@gmail.com>
2025-10-13 16:22:12 -0500

    feat: :truck: file renamed
    renamed to sales_DTO
    
    none

--- src/DTOS/sales_DTO.js
+++ src/DTOS/sales_DTO.js
@@ -6,4 +6,5 @@ export const crerVenta_dto = [
     body("productos.*.nombre").isString().trim().notEmpty().withMessage("Product name is required"),
     body("productos.*.precio").isFloat({min: 1}).withMessage("Product price is required"),
     body("productos.*.cantidad").isInt({min: 1}).withMessage("Product quantity is required"),
+    body("total").isFloat({min: 1}).withMessage("Total is required")
 ]
