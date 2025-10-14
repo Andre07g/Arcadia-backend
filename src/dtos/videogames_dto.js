f130e6689c4668869ea43543bcb58f9ab3383011
Andre07g <>
2025-10-14 12:06:14 -0500

    feat: :sparkles: Add videogames DTO Created the videogames Data Transfer Object (DTO) for structured data handling and validation. by @Andre07g

---
+++ src/dtos/videogames_dto.js
@@ -0,0 +1,75 @@
+import { body, param } from "express-validator";
+
+export const createGameDTO = [
+    body("id")
+        .isInt({ gt: 0 })
+        .notEmpty()
+        .withMessage("The id must be an integer upper than 0"),
+
+    body("title")
+        .isString()
+        .trim()
+        .notEmpty()
+        .withMessage("The title must be the name of the game, a string"),
+
+    body("genre")
+        .isString()
+        .trim()
+        .notEmpty()
+        .withMessage("The genre must be a string"),
+    body("platform")
+        .isString()
+        .trim()
+        .notEmpty()
+        .withMessage("The platform must be a string"),
+    body("description")
+        .isString()
+        .trim()
+        .notEmpty()
+        .withMessage("The description must be a string"),
+    body("price")
+        .isInt({ gt: 0 })
+        .notEmpty()
+        .withMessage("The price must be an integer upper than 0"),
+    body("image")
+        .isString()
+        .trim()
+        .notEmpty()
+        .withMessage("The image must be the link of the game"),
+];
+
+
+
+export const updateGameDTO = [
+
+    body("title")
+        .isString()
+        .trim()
+        .notEmpty()
+        .withMessage("The title must be the name of the game, a string")
+        .optional(),
+    body("genre")
+        .isString()
+        .trim()
+        .notEmpty() .optional()
+        .withMessage("The genre must be a string"),
+    body("platform")
+        .isString()
+        .trim()
+        .notEmpty() .optional()
+        .withMessage("The platform must be a string"),
+    body("description")
+        .isString()
+        .trim()
+        .notEmpty() .optional()
+        .withMessage("The description must be a string"),
+    body("price")
+        .isInt({ gt: 0 })
+        .notEmpty() .optional()
+        .withMessage("The price must be an integer upper than 0"),
+    body("image")
+        .isString()
+        .trim()
+        .notEmpty() .optional()
+        .withMessage("The image must be the link of the game"),
+];
