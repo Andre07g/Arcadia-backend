70841e5269d5f9b8818b1dde5f6ce60cc2521d15
Sebas404040 <delgadopalitogarbage@gmail.com>
2025-10-15 14:36:16 -0500

    feat: :truck: DTO renamed
    renamed succesfully
    
    none

--- src/DTOS/sales_DTO.js
+++ src/DTOS/sales_DTO.js
@@ -1,6 +1,6 @@
 import {param, body} from "express-validator";
 
-export const crerVenta_dto = [
+export const createSale_DTO = [
     body("fecha").isISO8601().toDate().withMessage("Date has to be ISO8601"),
     body("productos").isArray({min: 1}).withMessage("Minimum 1 product is required"),
     body("productos.*.nombre").isString().trim().notEmpty().withMessage("Product name is required"),
